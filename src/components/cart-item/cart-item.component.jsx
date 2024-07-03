a7b6530473ed97fd2486d28b4335675c75797f28
sparkjames <jpfleiderer@gmail.com>
2024-07-03 13:01:05 -0400

    styled components for cart item

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,19 @@
-import './cart-item.styles.scss'
+// import './cart-item.styles.scss'
+import { CartItemContainer, CartItemImageContainer, CartItemDetails, CartItemName } from './cart-item.styles';
 
 const CartItem = ({cartItem}) => {
   const { id, name, imageUrl, price, quantity } = cartItem;
 
   return (
-    <div className='cart-item' key={id}>
-      <div className='cart-item-image-container'>
+    <CartItemContainer key={id}>
+      <CartItemImageContainer>
         <img className='cart-item-image' src={imageUrl} alt={name} />
-      </div>
-      <div className='cart-item-details'>
-        <span className="cart-item-name">{name}</span>
+      </CartItemImageContainer>
+      <CartItemDetails>
+        <CartItemName>{name}</CartItemName>
         <span className="cart-item-price">{quantity} x ${price}</span>
-      </div>
-    </div>
+      </CartItemDetails>
+    </CartItemContainer>
   );
 }
 
