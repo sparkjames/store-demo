c962b10d3d36f4eac18aa815cf90e448480722d7
sparkjames <jpfleiderer@gmail.com>
2024-07-09 13:06:01 -0400

    add firebase config back to repo, access is now restricted via firebase rules

---
+++ src/utilities/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA-rvIhFe_U0nakahNWxBalTOCjRKafSH4",
+  authDomain: "react-clothing-store-7985b.firebaseapp.com",
+  projectId: "react-clothing-store-7985b",
+  storageBucket: "react-clothing-store-7985b.appspot.com",
+  messagingSenderId: "181284933741",
+  appId: "1:181284933741:web:7d6f46c19b84ae7779af64"
+};
+
+export default firebaseConfig;
